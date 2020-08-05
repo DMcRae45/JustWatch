2ac86651b3407e1228127d450b17d339fac3b246
DMcRae45 <DMcRae45@gmail.com>
2020-08-05 02:00:49 +0100

    fixed logo size

--- View/navigation.php
+++ View/navigation.php
@@ -9,7 +9,7 @@
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-  <a href="index.php"><img src="Images/fire.png"></a>
+  <a href="index.php"><img src="Images/fire.png" width="50px" height="50px" class="rounded-circle"></a>
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
