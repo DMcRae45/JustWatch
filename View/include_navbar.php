0464a1acd7752b5cf6ed6aa371a3b9c07f18e9a0
Sandbagg45 <oliverdickens1990@gmail.com>
2020-08-05 18:59:55 +0100

    nav

--- View/include_navbar.php
+++ View/include_navbar.php
@@ -18,8 +18,20 @@
     <ul class="navbar-nav mr-auto">
 
         <li class="nav-item active">
-            <a class="nav-link" href="index.php">Movies <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="index.php">Burner Movies <span class="sr-only">(current)</span></a>
         </li>
+
+        <li class="nav-item active">
+          <a class="nav-link" href="movies.php">Movies <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="series.php">Series <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="genre.php">Genre <span class="sr-only">(current)</span></a>
+        </li>
+
+
 
         <li class="nav-item dropdown active">
 
