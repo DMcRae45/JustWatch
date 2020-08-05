b413fbcf73e24b6ab34a79d5edcc869ff9aaec92
Sandbagg45 <oliverdickens1990@gmail.com>
2020-08-05 19:26:27 +0100

    ui

--- View/include_navbar.php
+++ View/include_navbar.php
@@ -3,10 +3,12 @@
 /*
     Description: Dundee Picture House Navigation bar at the top of each page.
 
-    Author: David McRae, Brad Mair, Aaron Hay
+    Author: David McRae, Oliver Dickens
     Reference: Bootstrap
 */
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+
 
 </nav>
