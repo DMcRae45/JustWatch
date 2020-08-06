58a2ba2a1f3206c0b8246ad6a35508c8b0ead8c3
Sandbagg45 <oliverdickens1990@gmail.com>
2020-08-06 21:24:13 +0100

    small touch ups

--- View/play.php
+++ View/play.php
@@ -9,12 +9,14 @@
 <body>
 <?php
 echo "
+
   <div class='main' id='main' style='margin-left:25%; margin-top:5%' >
-      <div alt='Max-width 100%'>
-        <h1>".$movieArray->Title."</h1>
+      <div alt='Max-width 100% '>
+        <h1 class='primaryDark'>".$movieArray->Title."</h1>
         <video width='1000' height='500' src='".$movieArray->Video_link."' autoplay controls/>
       </div>
   </div>
+
 ";
 ?>
 <!-- </footer> -->
