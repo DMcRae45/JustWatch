c7d09fa75ba823d3180f0d59e365dba376d68ee4
DMcRae45 <DMcRae45@gmail.com>
2020-08-06 14:24:05 +0100

    alterations to play file and styling

--- View/play.php
+++ View/play.php
@@ -5,6 +5,7 @@
 */
 
   Include 'include_header.php';
+  Include '../Controller/getMovieByID.php';
 ?>
 
 <?php
@@ -12,10 +13,8 @@ echo "
 <body>
   <div class='main' id='main' style='margin-left:25%; margin-top:5%' >
       <div alt='Max-width 100%'>
-        <h1 class='w3-xxxlarge w3-text-white'><b>Movie Title Here</b></h5>
-        <video width='1000' height='500'  src='Movies/PlayerOne.mp4' autoplay controls>
-          <source src='Movies/PlayerOne.mp4' type='video/mp4'>
-        </video>
+        <h1>".$movieArray->Title."</h1>
+        <video width='1000' height='500' src='".$movieArray->Video_link."' autoplay controls/>
       </div>
   </div>
 </body>
