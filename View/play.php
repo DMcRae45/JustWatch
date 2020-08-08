7e584ae760d7c18a39de9ddfafde1d8018110a36
Sandbagg45 <oliverdickens1990@gmail.com>
2020-08-08 14:27:18 +0100

    play page inline

--- View/play.php
+++ View/play.php
@@ -11,14 +11,12 @@
   <header class="showcase">
     <?php
     echo "
-
-      <div class='main' id='main' style='margin-left:25%; margin-top:5%' >
+      <div class='main' id='main' style='margin-left:25%; margin-top:15%' >
           <div alt='Max-width 100% '>
             <h1 class='primaryDark'>".$movieArray->Title."</h1>
             <video width='1000' height='500' src='".$movieArray->Video_link."' autoplay controls/>
           </div>
       </div>
-
     ";
     ?>
   </header>
