d1c2560549c31629185dfa07d7ee9479c6dcc403
Sandbagg45 <oliverdickens1990@gmail.com>
2020-09-21 20:51:25 +0100

    oli: Implementing sidenav

--- View/header.php
+++ View/header.php
@@ -12,7 +12,7 @@
 
   <!-- image for tabs in browser -->
   <link rel="icon" href="images/fire.png">
-
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet"/>
   <!-- bootstrap Css -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
