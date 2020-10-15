1698e351608841d8cc6b344901c5db60ac61f854
DMcRae45 <DMcRae45@gmail.com>
2020-10-15 21:52:02 +0100

    split files to tidy pageNav

--- View/movies.php
+++ View/movies.php
@@ -50,6 +50,8 @@
             <div class="container text-center mt-2">
               <h1>Movies</h1><hr>
               <?php
+              include 'pageNavigation.php';
+
                 // USE THIS VERSION FROM NOW ON
               $rows = 0;
               $cols = 10;
@@ -89,71 +91,11 @@
                   echo'<div class="col-md-4">&nbsp;</div>';
                 }
               }
-              echo '</div></div><br>';
+              echo "</div>";
+              include 'pageNavigation.php';
+              echo "</div><br>";
               ?>
-              <?php
-              if(!isset($_GET['page']))
-              {
-                $page = 1;
-              }
-              else
-              {
-                $page = $_GET['page'];
-              }
 
-
-                  echo "
-                  <div>
-                    <ul class='pagination'>
-                      <li class='page-item'>
-                        ";
-                        if($page < 2)
-                        {
-                          echo "
-                          <a class='page-link disabled' href='movie.php?page=".($page -1)."' aria-label='nob'>
-                          <span>Previous</span>
-                          </a>
-                          ";
-                        }
-                        else
-                        {
-                          echo "
-                          <a class='page-link' href='movies.php?page=".($page -1)."' aria-label='Dick'>
-                          <span>Previous</span>
-                          </a>
-                          ";
-                        }
-                      echo "
-                      </li>
-                      <li class='page-item'><a class='page-link' href='movies.php?page=".($page -5)."'>".($page -5)."</a></li>
-                      <li class='page-item'><a class='page-link' href='movies.php?page=".($page -2)."'>".($page -2)."</a></li>
-                      <li class='page-item'><a class='page-link' href='#'>".$page."</a></li>
-                      <li class='page-item'><a class='page-link' href='movies.php?page=".($page +2)."'>".($page +2)."</a></li>
-                      <li class='page-item'><a class='page-link' href='movies.php?page=".($page +5)."'>".($page +5)."</a></li>
-                      <li class='page-item'>
-                      ";
-                      if($page > 8)
-                      {
-                        echo "
-                        <a class='page-link disabled' href='#' aria-label='Next'>
-                          <span>Next</span>
-                        </a>
-                        ";
-                      }
-                      else
-                      {
-                        echo "
-                        <a class='page-link' href='movies.php?page=".($page +1)."' aria-label='Next'>
-                          <span>Next</span>
-                        </a>
-                        ";
-                      }
-                      echo "
-                      </li>
-                    </ul>
-                  </div>
-                  ";
-                ?>
 
           <?php
           include 'footer.php';
