a1916428054c51bc9cc160f1282b39f6b7773591
DMcRae45 <DMcRae45@gmail.com>
2020-10-20 20:21:47 +0100

    made 5 per row

--- View/movies.php
+++ View/movies.php
@@ -54,25 +54,27 @@
               <h1>Movies</h1><hr>
               <?php
               include 'pageNavigation.php';
+              //TODO: Expansion: add Bookmarked button to add to watch list
 
-                // USE THIS VERSION FROM NOW ON
-              $rows = 0;
-              $cols = 5;
-              $counter = 1;
-              $nbsp = $cols - ($rows % $cols);
+
+              $perRow = 5;
+              $totalItems = sizeof($movieArray);
+              $currentItem = 0;
+
+              $rows = $totalItems / $perRow;
+              $countPerRow = 0;
+
               for ($i=0 ; $i < sizeof($movieArray) ; $i++)
               {
-
-                if(($counter % $cols) == 1)
+                if($countPerRow === 0)
                 {
-                  echo '<div class="row">';
+                  echo "<div class='row'>";
                 }
                 echo "<div class='col'>"; // open col
                 echo "<div class='card'>"; // Open card div
                 echo "<div class='poster' >"; // Open card poster
                 echo "<a href='play.php?id=".$movieArray[$i]->Movie_ID."'> <img src='".$movieArray[$i]->Image_link."'  alt='".$movieArray[$i]->Movie_ID."' onerror=this.src='images/film.placeholder.poster.jpg'></a>"; // card image
                 echo "</div>";// close poster
-                //TODO: Expansion: add Bookmarked button to add to watch list
                 echo "<div class='card-bottom'>";
                 echo "<class='movieTitle'>".$movieArray[$i]->Title."<br>";
                 echo "<class='movieTitle'>".$movieArray[$i]->Year."";
@@ -80,20 +82,15 @@
                 echo "</div>";// close card
                 echo "</div>";// close col
 
-                if(($counter % $cols) == 0)
+                $countPerRow++;
+
+                if($countPerRow == 5 || $currentItem == $totalItems)
                 {
-                  echo '</div>';
+                  $countPerRow = 0;
+                  echo "</div>";
                 }
-                $counter++;
               }
 
-              if($nbsp > 0)
-              {
-                for ($i = 0; $i < $nbsp; $i++)
-                {
-                  echo'<div class="col-md-4">&nbsp;</div>';
-                }
-              }
               include 'pageNavigation.php';
             echo "
             </div>
